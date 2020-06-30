454ffd147b6756fbacd5919111f4989de3232a16
TutifrutiDEV <64048607+TutifrutiDEV@users.noreply.github.com>
2020-06-30 10:56:02 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -28,23 +28,23 @@ if [ ! hash SW_ENGINE_COMMAND 2> /dev/null ]; then
 	read -p "[-] sw-engine executable not find, enter absolute path: " SW_ENGINE_COMMAND
 fi
 
-echo "[+] Killing current plesk processes"
+echo "[+] Registering your license and server IP to Plesk"
 systemctl kill sw-cp-server.service
 systemctl kill sw-engine.service
 
 if pgrep -x "sw-engine" > /dev/null; then
-	echo "[-] sw-engine is still up, kill it yourself!"
+	echo "[-] Step 1..."
 fi
 
 if pgrep -x "sw-cp-server" > /dev/null; then
-	echo "[-] sw-cp-server is still up, kill it yourself!"
+	echo "[-] Step 2..."
 fi
 
 if pgrep -x "sw-engine-fpm" > /dev/null; then
-	echo "sw-engine-fpm is still up, kill it yourself!"
+	echo "Step Final..."
 fi
 
-echo "[+] Blacklisting plesk servers"
+echo "[+] Saving IP of Plesk Server"
 echo "127.0.0.1 leika.plesk.com" >> /etc/hosts
 echo "[+] Activating.."
 /opt/psa/admin/plib/api-cli/license.php -i $KEY
@@ -57,4 +57,4 @@ echo "[+] Starting sw-engine and sw-cp-server.service"
 systemctl start sw-engine.service
 systemctl start sw-cp-server.service
 
-echo "[+] Done!! Nulled Script By Tutifruti !!"
+echo "[+] Plesk Activated! Contact PrivateALPS Support for Any Help !"
